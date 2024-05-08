6cf3d58e1a9f5138bc1500bd4295ca055a11f15d
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-05-09 00:52:58 +0300

    get products by category id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProduct, getAllProducts } = require("../controllers/products");
+const { createProduct, getAllProducts, getProductsByCategoryId } = require("../controllers/products");
 
 //authentication and authorization
 const authentication = require("../middlewares/authentication");
@@ -9,4 +9,5 @@ const productsRouter = express.Router();
 
 productsRouter.post("/",authentication, createProduct);
 productsRouter.get("/",getAllProducts)
+productsRouter.get("/:id",getProductsByCategoryId)
 module.exports = productsRouter;
