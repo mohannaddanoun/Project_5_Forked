2a851389ecd1c6acca42194b257260fcb460fe9e
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-05-09 21:53:33 +0300

    update authoriztion

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -1,13 +1,16 @@
-const pool = require("../models/db");
+const {pool} = require("../models/db");
 
 const authorization = (string) => {
   return function (req, res, next) {
     const role_id = req.token.role;
     const data = [role_id, string];
-    const query = `SELECT * FROM role_permission RP INNER JOIN permissions P ON RP.permission_id = P.id WHERE RP.role_id = ($1) AND P.permission = ($2)`;
+    const query = `SELECT permission FROM role_permission AS RP INNER JOIN permissions AS P ON RP.permission_id = P.id WHERE RP.id = $1 AND P.permission= $2`;
+    console.log(data);
+    console.log(query);
     pool
-      .query(query, data)
+      .query(query,data)
       .then((result) => {
+        console.log(result);
         if (result.rows.length) {
           next();
         } else {
@@ -15,7 +18,8 @@ const authorization = (string) => {
         }
       })
       .catch((err) => {
-        res.status(400).json({ message: "unauthorized" });
+        res.status(400).json({ message: "unauthorized" ,err :err})
+        console.log(err);
       });
   };
 };
