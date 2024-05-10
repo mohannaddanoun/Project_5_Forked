d167bd4eeee71d276a588d3714b8faa3833184e3
mohannaddanoun <danoun.mohanad@gmail.com>
2024-05-10 15:49:47 +0300

    get comments by product id

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -26,9 +26,45 @@ const createNewComment = (req,res)=>{
 
 
 }
+// This function returns the comments by product Id
 
+const getCommentsByProduct =(req,res)=>{
+
+  const productId= req.params.id
+  const isDeleted=0 
+  pool.query(`SELECT comments.comment , comments.product_id , comments.commenter_id ,users.userName 
+  FROM comments
+  INNER JOIN users ON comments.commenter_id=users.id
+  WHERE comments.product_id= $1 AND comments.is_deleted = $2`,[productId,isDeleted])
+  .then((result) => {
+    if (!result.rows.length) {
+      res.status(404).json({
+        success: false,
+        message: `No comment yet for product: ${productId} `,
+      });
+    } else {
+    res.status(200).json({
+      success: true,
+      message: `All comments for product: ${productId}`,
+      comments: result.rows
+    });
+  }
+  })
+  .catch((err) => {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      err: err.message
+    });
+  });
+
+
+
+
+}
 
 module.exports ={
-    createNewComment
+    createNewComment,
+    getCommentsByProduct
 
 }
