1b908660e0cc5e1283d6c40002bc7061e61160c3
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-05-10 20:30:21 +0300

    update product

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -4,7 +4,7 @@ const authorization = (string) => {
   return function (req, res, next) {
     const role_id = req.token.role;
     const data = [role_id, string];
-    const query = `SELECT permission FROM role_permission AS RP INNER JOIN permissions AS P ON RP.permission_id = P.id WHERE RP.id = $1 AND P.permission= $2`;
+    const query = `SELECT permission FROM role_permission AS RP INNER JOIN permissions AS P ON RP.permission_id = P.id WHERE RP.role_id = $1 AND P.permission= $2`;
     console.log(data);
     console.log(query);
     pool
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createProduct, getAllProducts, getProductsByCategoryId } = require("../controllers/products");
+const { createProduct, getAllProducts, getProductsByCategoryId, updateProduct } = require("../controllers/products");
 
 //authentication and authorization
 const authentication = require("../middlewares/authentication");
@@ -10,4 +10,5 @@ const productsRouter = express.Router();
 productsRouter.post("/",authentication,authorization("CREATE_PRODUCTS"), createProduct);
 productsRouter.get("/",getAllProducts)
 productsRouter.get("/:id",getProductsByCategoryId)
+productsRouter.put("/:id",authentication,authorization("CREATE_PRODUCTS"),updateProduct)
 module.exports = productsRouter;
