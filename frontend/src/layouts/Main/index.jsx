52b6517eca160b65753eada2a450a8d622d5701d
Ahmadalshishani <ahmad.alshishanii@gmail.com>
2024-05-24 21:47:55 +0300

    log in style

--- frontend/src/layouts/Main/index.jsx
+++ frontend/src/layouts/Main/index.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/NavBar";
 import Footer from "../../components/Footer";
-
+import "./style.css"
 export default function Main() {
   return (
     <div className="root-layout" style={{ display: "flex", flexDirection: "column", minHeight: "100vh", maxWidth:"auto" }}>
